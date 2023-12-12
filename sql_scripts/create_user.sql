295ae5a4f17834eb9137e16606f2d31c926e99b9
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-12-12 15:00:48 +0600

    code refactored

--- sql_scripts/create_user.sql
+++ sql_scripts/create_user.sql
@@ -2,30 +2,42 @@
 USE click_bit;
 
 -- Create users table
-CREATE TABLE users (
-    ID INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
-    password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
-    type VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
-    active TINYINT DEFAULT 1,
-    PRIMARY KEY (ID)
-);
+CREATE TABLE
+    users (
+        ID INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) CHARACTER
+        SET
+            utf8mb4 NOT NULL,
+            password VARCHAR(255) CHARACTER
+        SET
+            utf8mb4 NOT NULL,
+            type VARCHAR(255) CHARACTER
+        SET
+            utf8mb4 NOT NULL,
+            active TINYINT DEFAULT 1,
+            PRIMARY KEY (ID)
+    );
 
 -- create the addUser procedure and store it
-DELIMITER //
-CREATE PROCEDURE addUser(
+DELIMITER / / CREATE PROCEDURE addUser (
     IN userEmail VARCHAR(255),
     IN userPassword VARCHAR(255),
     IN userType VARCHAR(255)
-)
-BEGIN
-    -- Insert a new user into the users table
-    INSERT INTO users (email, password, type) VALUES (userEmail, userPassword, userType);
-END //
-DELIMITER ;
+) BEGIN
+-- Insert a new user into the users table
+INSERT INTO
+    users (email, password, type)
+VALUES
+    (userEmail, userPassword, userType);
+
+END / / DELIMITER;
 
 -- call addUser to insert  new user
-CALL addUser('md.muktadirul.haque.maruf@gmail.com', 'password123', 'regular');
+CALL addUser (
+    'md.muktadirul.haque.maruf@gmail.com',
+    'password123',
+    'regular'
+);
 
 -- show a list of all tables
 -- SHOW TABLES;
